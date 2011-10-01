0e8087b5c946d12f0ea5720434cb2b89e018a748
Denis Doria <denisdoria@gmail.com>
2011-10-01 18:04:17 -0300

    Daemon works fines, signal to stop it (kill 15) works fine.

--- src/mdd.py
+++ src/mdd.py
@@ -22,17 +22,24 @@ Copyright (C) 2011 Denis 'Thuck' Doria
 # -------------------------------------------------------------------------
 
 from optparse import OptionParser
+import os
 import daemon
 import daemon.pidlockfile as pid
+from daemon.pidlockfile import PIDFileParseError
 import sys
 from core import MagicDirectory
 import time
+import signal
 
 RUN = True
 
+def stop_daemon(signal, stack):
+    global RUN
+    RUN = False
+
 def parse_args():
     parser = OptionParser()
-    parser.add_option("-c", "--conf", dest="conf", default='~/.config/mdd/conf'
+    parser.add_option("-c", "--conf", dest="conf", default='~/.config/mdd/conf',
                   help="MDD configuration file, default: [~/.config/mdd/conf]")
     parser.add_option("-i", "--interval",
                   action="store", dest="interval", default=60,
@@ -48,7 +55,6 @@ def parse_args():
                   action="store_true", dest="version", default=False,
                   help="Print the current version.")
 
-
     (options, args) = parser.parse_args()
 
     return options
@@ -59,15 +65,34 @@ if __name__ == '__main__':
     if options.version:
         print '0.1'
         sys.exit(0)
-        
-    #TODO: create the pid control
+
+    pid_path = '/var/lock/%s_mdd.pid' % (os.getlogin())
+
+    try:
+        pid_file = pid.read_pid_from_pidfile(pid_path)
+
+    except PIDFileParseError:
+        print "Pid file doesn't contain a valid pid, solve this and try again"
+        sys.exit(1)
+
+    if pid_file is not None:
+        print "MDD is running already"
+        sys.exit(1)
         
     magic_directory = MagicDirectory(options.conf, options.section)
 
     if options.no_daemon:
+        pid.write_pid_to_pidfile(pid_path)
         magic_directory.run()
+        pid.remove_existing_pidfile(pid_path)
+
     else:
         with daemon.DaemonContext():
+            signal.signal(signal.SIGTERM, stop_daemon)
+            pid.write_pid_to_pidfile(pid_path)
             while RUN:
                 magic_directory.run()
                 time.sleep(options.interval)
+            pid.remove_existing_pidfile(pid_path)
+
+    sys.exit(0)
